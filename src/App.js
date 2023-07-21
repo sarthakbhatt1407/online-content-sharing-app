7eec2d27101d12e8413fca079af34e79e68a1072
sarthakbhatt1407 <sarthakbhatt1407@gmail.com>
2023-07-21 21:27:09 +0530

    updated post

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import MainHeader from "./components/Header/MainHeader";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
@@ -15,9 +15,62 @@ import { useDispatch, useSelector } from "react-redux";
 import PostFullView from "./pages/PostFullView";
 import UserProfile from "./pages/UserProfile";
 import AccountSetting from "./pages/AccountSetting";
+import styled, { keyframes } from "styled-components";
+import Modal from "./UI/Modal";
+import AddNewPost from "./components/AddNewPost";
 
+const MainBox = styled.div`
+  position: relative;
+`;
+
+const BtnAni = keyframes`
+0%{opacity:0}
+100%{
+  opacity: 1;
+}
+`;
+
+const AddNewButton = styled.div`
+  position: absolute;
+  background-color: #1a6ed8;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 3.2rem;
+  height: 3.2rem;
+  bottom: 5%;
+  right: 5%;
+  color: white;
+  transition: all 0.3s;
+  z-index: 10;
+  p {
+    display: inline;
+    margin-bottom: 1.3rem;
+    font-size: 1.2rem;
+    font-weight: bold;
+  }
+  span {
+    display: none;
+  }
+  &:hover {
+    border-radius: 0.8rem;
+    width: 8rem;
+    p {
+      display: none;
+    }
+    span {
+      display: inline;
+      animation: ${BtnAni} 0.5s;
+    }
+  }
+  @media (max-width: 750px) {
+    bottom: 7%;
+  }
+`;
 const App = () => {
   const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch();
   useEffect(() => {
     const userData = JSON.parse(localStorage.getItem("userData"));
@@ -25,11 +78,22 @@ const App = () => {
       dispatch({ type: "login", data: { ...userData } });
     }
   }, []);
+  const modalHandler = () => {
+    setShowModal(!showModal);
+  };
   return (
-    <div>
+    <MainBox>
+      <AddNewButton onClick={modalHandler}>
+        <p>+</p>
+        <span>Add New Post</span>
+      </AddNewButton>
+      {showModal && (
+        <Modal onClick={modalHandler}>
+          <AddNewPost onClick={modalHandler} />
+        </Modal>
+      )}
       <MainHeader />
       <Routes>
-        {" "}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot-password" element={<PasswordReset />} />
@@ -53,7 +117,7 @@ const App = () => {
           </>
         )}
       </Routes>
-    </div>
+    </MainBox>
   );
 };
 
--- src/components/AddNewPost.js
+++ src/components/AddNewPost.js
@@ -2,6 +2,35 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { EnvVariables } from "../data";
+
+const MainBox = styled.div`
+  height: 40vh;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+  justify-content: center;
+  input {
+    width: 40%;
+    padding: 0.6rem 2rem;
+    border: 1px solid #e8e8e8;
+    border-radius: 0.3rem;
+    box-shadow: 0.1rem 0.2rem 0.5rem #eaeaea;
+  }
+  div {
+    display: flex;
+    gap: 1rem;
+    button {
+      border: none;
+      background-color: #1a6ed8;
+      color: white;
+      padding: 0.3rem 1rem;
+      font-size: 1rem;
+      border-radius: 0.6rem;
+    }
+  }
+`;
+
 const ImgDiv = styled.div`
   padding: 0.6rem 2rem;
   border: 1px solid #e8e8e8;
@@ -12,8 +41,9 @@ const ImgDiv = styled.div`
   background-size: contain;
   background-repeat: no-repeat;
   background-position: center;
+  width: 40%;
 `;
-const AddNewPost = () => {
+const AddNewPost = (props) => {
   const dispatch = useDispatch();
   const [img, setImg] = useState("");
   const userId = useSelector((state) => state.userId);
@@ -73,12 +103,12 @@ const AddNewPost = () => {
     setInputFields(defaultFields);
   };
   return (
-    <div>
+    <MainBox>
       <input
         type="text"
         id="desc"
         onChange={onChangeHandler}
-        placeholder="Enter Description"
+        placeholder="Enter Message"
       />
       <input
         type="file"
@@ -87,8 +117,11 @@ const AddNewPost = () => {
         onChange={imageOnChangeHandler}
       />
       {img && <ImgDiv image={img}></ImgDiv>}
-      <button onClick={onClickHandler}>Submit</button>
-    </div>
+      <div>
+        <button onClick={onClickHandler}>Submit</button>
+        <button onClick={props.onClick}>Cancel</button>
+      </div>
+    </MainBox>
   );
 };
 
